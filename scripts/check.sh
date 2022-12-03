e116bc1855d6e9284a714fa3f2cd01a3be510bbe
vwkd <33468089+vwkd@users.noreply.github.com>
2022-12-03 22:53:49 +0200

    scripts: improve check

--- scripts/check.sh
+++ scripts/check.sh
@@ -10,12 +10,15 @@
 # - verb lines starting with two spaces
 
 # removes:
+# - infinitive suffix on verb root (needs gnu-sed because of -z option)
+# - rest of line after first space
 # - hyphen
 # - parentheses
 # - vertical bar
 # - bold markdown formatting
 
 head -n $1 src/dict.txt \
+| gsed -z -e "s/\*\*[^\*]*\n  /\n  /g" \
 | grep -v -e "^##" -e "^$" -e "^  " \
-| sed -e "s/-//g" -e "s/(//g" -e "s/)//g" -e "s/|//g" -e "s/\*\*//g" \
-| sort -c -s -k 1,1
+| sed -e "s/ .*$//g" -e "s/-//g" -e "s/(//g" -e "s/)//g" -e "s/|//g" -e "s/\*\*//g" \
+| LC_ALL=C sort -c
