850ba0d22f84b789ed81f75292f4fd8021c734ec
vwkd <33468089+vwkd@users.noreply.github.com>
2022-12-05 20:26:55 +0200

    scripts: fix check

--- scripts/check.sh
+++ scripts/check.sh
@@ -11,7 +11,8 @@
 # - first line on page if continued from previous page
 
 # removes:
-# - infinitive suffix on verb root (needs gnu-sed because of -z option)
+# - rest of line after last star if next line start with two spaces (infinitive suffix on verb root)
+#   uses gnu-sed because of -z option
 # - rest of line after first space
 # - hyphen
 # - parentheses
@@ -22,7 +23,7 @@
 # - superscript numbers with numbers (because `LC_ALL=C sort` puts superscript numbers in wrong order)
 
 head -n $1 src/dict.txt \
-| gsed -z -e "s/\*\*[^\*]*\n  /\n  /g" \
+| gsed -z -E "s/([^ ]*\*)([^\n]*)(\n  )/\1\3/g" \
 | grep -v -e "^##" -e "^$" -e "^  " -e "^♦︎" \
 | sed -e "s/ .*$//g" -e "s/-//g" -e "s/(//g" -e "s/)//g" -e "s/|//g" -e "s/\*\*//g" -e "s/¹/1/g" -e "s/²/2/g" -e "s/³/3/g" -e "s/⁴/4/g" -e "s/⁵/5/g" -e "s/⁶/6/g" -e "s/⁷/7/g" -e "s/⁸/8/g" -e "s/⁹/9/g" \
 | LC_ALL=C sort -c
