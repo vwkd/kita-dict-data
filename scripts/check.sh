15268022af8636f598caf3e9eca5442fc61d2292
vwkd <33468089+vwkd@users.noreply.github.com>
2022-12-12 20:54:00 +0200

    scripts: fix order hyphen

--- scripts/check.sh
+++ scripts/check.sh
@@ -34,6 +34,7 @@ head -n $1 src/dict.txt \
 
 # checks if entries are sorted alphabetically
 # errors on first unsorted entry
+# defines: first -, then letter, then numbers
 
 # filters out:
 # - header lines starting with two hashes
@@ -45,7 +46,9 @@ head -n $1 src/dict.txt \
 # - rest of line after last star if next line start with two spaces (infinitive suffix on verb root)
 #   uses gnu-sed because of -z option
 # - rest of line after first space
-# - hyphen
+# - hyphen, except
+#   - if at end keep
+#   - if at start add placeholder `@` to sort last
 # - parentheses
 # - vertical bar
 # - bold markdown formatting
@@ -57,5 +60,5 @@ echo "Checking incorrect sort..."
 head -n $1 src/dict.txt \
 | gsed -z -E "s/([^ ]*\*[¹²³⁴⁵⁶⁷⁸⁹]?)([^\n]*)(\n  )/\1\3/g" \
 | grep -v -e "^##" -e "^$" -e "^  " -e "^♦︎" \
-| sed -e "s/ .*$//g" -e "s/-//g" -e "s/(//g" -e "s/)//g" -e "s/|//g" -e "s/\*\*//g" -e "s/¹/1/g" -e "s/²/2/g" -e "s/³/3/g" -e "s/⁴/4/g" -e "s/⁵/5/g" -e "s/⁶/6/g" -e "s/⁷/7/g" -e "s/⁸/8/g" -e "s/⁹/9/g" \
+| sed -E -e "s/ .*$//g" -e "s/^-(.+)/\1@/g" -e "s/(.)-(.)/\1\2/g" -e "s/\(//g" -e "s/\)//g" -e "s/\|//g" -e "s/\*\*//g" -e "s/¹/1/g" -e "s/²/2/g" -e "s/³/3/g" -e "s/⁴/4/g" -e "s/⁵/5/g" -e "s/⁶/6/g" -e "s/⁷/7/g" -e "s/⁸/8/g" -e "s/⁹/9/g" \
 | LC_ALL=C sort -c
