fce6f438905f9fa6c31ccae05111a14b491f7bc3
vwkd <33468089+vwkd@users.noreply.github.com>
2023-02-01 23:19:35 +0200

    scripts: fix not preceeded by hyphen

--- scripts/merge_lines.sh
+++ scripts/merge_lines.sh
@@ -7,7 +7,7 @@
 # - line ends in `~-`
 # - line ends in `zs.-`
 # - line ends in `-` and next line starts with uppercase, e.g. `Trauben-\nArt`
-# - line ends in `-` and next line starts with lowercase word, comma, whitespace, hyphen, lowercase word, e.g. `auf-\ngehen, -legen`
+# - line ends in `-` and preceeded by not hyphen (currently space), lowercase word, next line starts with lowercase word, comma, whitespace, hyphen, lowercase word, e.g. `auf-\ngehen, -legen`
 # - line ends in `-` and next line start with `ea.`, e.g. `hinter-\nea.`
 # - line ends in `-` and preceeded by hyphen, comma, space and characters, and followed by characters, not hyphen (currently space / comma / semicolon / line-end), e.g. `Waschbär-, Schuppen-\npelz`, but not `Zusammen-, Gemein-\nschafts-leben`
 # - line ends in `/`
@@ -24,7 +24,7 @@ gsed -z -E \
   -e "s/(~-)\n(.)/\1\2/g" \
   -e "s/(zs\.-)\n(.)/\1\2/g" \
   -e "s/(-)\n([A-ZÄÖÜẞ])/\1\2/g" \
-  -e "s/(-)\n([a-zäöüß]+, -[a-zäöüß])/\1\2/g" \
+  -e "s/([ ][a-zäöüß]+-)\n([a-zäöüß]+, -[a-zäöüß])/\1\2/g" \
   -e "s/(-)\n(ea\.)/\1\2/g" \
   -e "s/([a-zäöüß]-, [A-ZÄÖÜẞ]?[a-zäöüß]+-)\n([a-zäöüß]+([ ,;\n]))/\1\2/g" \
   -e "s/(\/)\n(.)/\1\2/g" \
