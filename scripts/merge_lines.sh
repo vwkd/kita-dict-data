d7670029849c3c9642b39e3f20d3bd01edcf800a
vwkd <33468089+vwkd@users.noreply.github.com>
2023-04-16 13:39:35 +0400

    scripts: improve merge lines

--- scripts/merge_lines.sh
+++ scripts/merge_lines.sh
@@ -9,7 +9,7 @@
 # - line ends in `-` and next line starts with uppercase, e.g. `Trauben-\nArt`
 # - line ends in `-` and preceeded by not hyphen (currently space), lowercase word, next line starts with lowercase word, comma, whitespace, hyphen, lowercase word, e.g. `auf-\ngehen, -legen`
 # - line ends in `-` and next line start with `ea.`, e.g. `hinter-\nea.`
-# - line ends in `-` and preceeded by hyphen, comma, space and characters, and followed by characters, not hyphen (currently space / comma / semicolon / line-end), e.g. `Waschbär-, Schuppen-\npelz`, but not `Zusammen-, Gemein-\nschafts-leben`
+# - line ends in `-` and preceeded by hyphen, comma, space and characters, and followed by characters, not hyphen (currently space / comma / semicolon / slash / line-end), e.g. `Waschbär-, Schuppen-\npelz`, but not `Zusammen-, Gemein-\nschafts-leben`
 # - line ends in `/`
 # - line ends in `-)` preceeded by non-space
 # - line ends in `)` preceeded by `(`, uppercase, and lowercase and next line starts with uppercase, e.g. `(Kegel)\nBahn`, but not `(in Zusammensetzungen) Schüler`
@@ -26,7 +26,7 @@ gsed -z -E \
   -e "s/(-)\n([A-ZÄÖÜẞ])/\1\2/g" \
   -e "s/([ ][a-zäöüß]+-)\n([a-zäöüß]+, -[a-zäöüß])/\1\2/g" \
   -e "s/(-)\n(ea\.)/\1\2/g" \
-  -e "s/([a-zäöüß]-, [A-ZÄÖÜẞ]?[a-zäöüß]+-)\n([a-zäöüß]+([ ,;\n]))/\1\2/g" \
+  -e "s/([a-zäöüß]-, [A-ZÄÖÜẞ]?[a-zäöüß]+-)\n([a-zäöüß]+([ ,;\/\n]))/\1\2/g" \
   -e "s/(\/)\n(.)/\1\2/g" \
   -e "s/([^ ]-\))\n(.)/\1\2/g" \
   -e "s/(\([A-ZÄÖÜẞ][a-zäöüß]+\))\n([A-ZÄÖÜẞ])/\1\2/g" \
