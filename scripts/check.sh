c49dd39a3312c371fad72981e977d187cb417ea1
vwkd <33468089+vwkd@users.noreply.github.com>
2023-12-30 02:17:25 +0400

    scripts/check: use single quotes

--- scripts/check.sh
+++ scripts/check.sh
@@ -5,64 +5,64 @@ echo "Running checks until line $1..."
 
 echo "Checking illegal characters..."
 head -n $1 src/dict.txt \
-| grep -n -o -E "[^][♦︎0-9¹²³⁴⁵⁶⁷⁸⁹½⅛⅝⁄₁₂₃₄₅₆₇₈₉ \(\)\|\.,:;~?!\/#\"'\*§=†Ωδέéàêëაბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰa-zäöüßA-ZÄÖÜẞ-]"
+| grep -n -o -E '[^][♦︎0-9¹²³⁴⁵⁶⁷⁸⁹½⅛⅝⁄₁₂₃₄₅₆₇₈₉ \(\)\|\.,:;~?!\/#"'\''\*§=†Ωδέéàêëაბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰa-zäöüßA-ZÄÖÜẞ-]'
 
 echo "Checking misrecognized characters..."
 head -n $1 src/dict.txt \
 | grep -n -E \
-  -e "~[a-zäöüßA-ZÄÖÜ]" \
-  -e "[a-zäöüßA-ZÄÖÜ]~"
+  -e '~[a-zäöüßA-ZÄÖÜ]' \
+  -e '[a-zäöüßA-ZÄÖÜ]~'
 
 echo "Checking mixed characters..."
 # no separation
 head -n $1 src/dict.txt \
 | grep -n -E \
-  -e "[a-zäöüßA-ZÄÖÜ][აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ]" \
-  -e "[აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ][a-zäöüßA-ZÄÖÜ]"
+  -e '[a-zäöüßA-ZÄÖÜ][აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ]' \
+  -e '[აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ][a-zäöüßA-ZÄÖÜ]'
 # separated by space
 head -n $1 src/dict.txt \
-| ggrep -n -P "[a-zäöüßA-ZÄÖÜ]-[აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ]"
+| ggrep -n -P '[a-zäöüßA-ZÄÖÜ]-[აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ]'
 head -n $1 src/dict.txt \
-| ggrep -n -P "[აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ]-(?!Spiel|weise|Massen|Brot|Instruments|Sänger|Partei|Tänzer)[a-zäöüßA-ZÄÖÜ]"
+| ggrep -n -P '[აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ]-(?!Spiel|weise|Massen|Brot|Instruments|Sänger|Partei|Tänzer)[a-zäöüßA-ZÄÖÜ]'
 
 # note: regex alternations to work around single pattern limitation of `-P` option
 echo "Checking missing superscript number..."
 head -n $1 src/dict.txt \
-| ggrep -n -P "(IV[^¹²³⁴\.])|(P[^¹²³\.a-zäöüR]])|(RM[^¹²³⁴])|(RP[^¹²³⁴⁵⁶⁷])|((?<!K)T[^¹²³⁴⁵\.a-zaäöü])|(ZP[^¹²³])"
+| ggrep -n -P '(IV[^¹²³⁴\.])|(P[^¹²³\.a-zäöüR]])|(RM[^¹²³⁴])|(RP[^¹²³⁴⁵⁶⁷])|((?<!K)T[^¹²³⁴⁵\.a-zaäöü])|(ZP[^¹²³])'
 
 echo "Checking merged lines..."
 # missing space after comma except if digit or newline
 head -n $1 src/dict.txt \
-| ggrep -nP ",(?! |\d|$)"
+| ggrep -nP ',(?! |\d|$)'
 # missing space after semicolon except if newline
 head -n $1 src/dict.txt \
-| ggrep -nP ";(?! |$)"
+| ggrep -nP ';(?! |$)'
 # `Inf.` or `3. Inf.` in middle of line
 head -n $1 src/dict.txt \
-| ggrep -n -P "(?<!(^ )|(^  \d\.)) Inf\."
+| ggrep -n -P '(?<!(^ )|(^  \d\.)) Inf\.'
 # hyphen followed by space except if first word in line
 # note: second inverse to work around non-fixed length negative lookbehind limitation
 # note: needs to include line numbers from first
 head -n $1 src/dict.txt \
-| ggrep -nP "[^ ]- (?!(u\. )|(od\. )|(und )|(oder )|(bzw\. )|(bis )|(usw\.\)))" \
-| grep -vE -e "^(\d+:)[აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ]+- "
+| ggrep -nP '[^ ]- (?!(u\. )|(od\. )|(und )|(oder )|(bzw\. )|(bis )|(usw\.\)))' \
+| grep -vE -e '^(\d+:)[აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ]+- '
 # hyphen followed by slash
 head -n $1 src/dict.txt \
-| grep -n -E ":\/"
+| grep -n -E ':\/'
 # slash followed by whitespace except if another slash with whitespace before, e.g. ` /s. unten/ `
 # note: second inverse to work around non-fixed length negative lookbehind limitation
 head -n $1 src/dict.txt \
-| grep -n -E "[^ ]\/ " \
-| grep -v -E " \/[\.!? აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰabcdefghijklmnopqrstuvwxyzäöüßABCD EFGHIJKLMNOPQRSTUVWXYZÄÖÜẞ]*\/ "
+| grep -n -E '[^ ]\/ ' \
+| grep -v -E ' \/[\.!? აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰabcdefghijklmnopqrstuvwxyzäöüßABCD EFGHIJKLMNOPQRSTUVWXYZÄÖÜẞ]*\/ '
 
 # except at start of line
 echo "Checking multiple space..."
 head -n $1 src/dict.txt \
-| grep -E "[^\n]\s{2,}"
+| grep -E '[^\n]\s{2,}'
 
 echo "Checking trailing space..."
 head -n $1 src/dict.txt \
-| grep -E " $"
+| grep -E ' $'
 
 # checks if entries are sorted alphabetically
 # errors on first unsorted entry
@@ -91,28 +91,28 @@ head -n $1 src/dict.txt \
 
 echo "Checking incorrect sort..."
 head -n $1 src/dict.txt \
-| gsed -z -E "s/([^ ]*\*[¹²³⁴⁵⁶⁷⁸⁹]?)([^\n]*)(\n  )/\1\3/g" \
+| gsed -z -E 's/([^ ]*\*[¹²³⁴⁵⁶⁷⁸⁹]?)([^\n]*)(\n  )/\1\3/g' \
 | grep -v \
-  -e "^##" \
-  -e "^$" \
-  -e "^  " \
-  -e "^♦︎" \
+  -e '^##' \
+  -e '^$' \
+  -e '^  ' \
+  -e '^♦︎' \
 | sed -E \
-  -e "s/(,)? .*$//g" \
-  -e "s/^-(.+)/\1@/g" \
-  -e "s/(.)-(.)/\1\2/g" \
-  -e "s/\(//g" \
-  -e "s/\)//g" \
-  -e "s/\|//g" \
-  -e "s/\*\*//g" \
-  -e "s/\.//g" \
-  -e "s/¹/1/g" \
-  -e "s/²/2/g" \
-  -e "s/³/3/g" \
-  -e "s/⁴/4/g" \
-  -e "s/⁵/5/g" \
-  -e "s/⁶/6/g" \
-  -e "s/⁷/7/g" \
-  -e "s/⁸/8/g" \
-  -e "s/⁹/9/g" \
+  -e 's/(,)? .*$//g' \
+  -e 's/^-(.+)/\1@/g' \
+  -e 's/(.)-(.)/\1\2/g' \
+  -e 's/\(//g' \
+  -e 's/\)//g' \
+  -e 's/\|//g' \
+  -e 's/\*\*//g' \
+  -e 's/\.//g' \
+  -e 's/¹/1/g' \
+  -e 's/²/2/g' \
+  -e 's/³/3/g' \
+  -e 's/⁴/4/g' \
+  -e 's/⁵/5/g' \
+  -e 's/⁶/6/g' \
+  -e 's/⁷/7/g' \
+  -e 's/⁸/8/g' \
+  -e 's/⁹/9/g' \
 | LC_ALL=C sort -c
