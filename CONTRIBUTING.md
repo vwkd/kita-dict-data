6e9205997c8233b520ecf05c35a2dce52328b4a8
vwkd <33468089+vwkd@users.noreply.github.com>
2024-01-01 12:04:28 +0400

    scripts/commit_hook: add

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -4,6 +4,12 @@
 
 ## Config
 
+- add git commit hook
+
+```sh
+ln -s "$PWD/scripts/commit_hook.sh" .git/hooks/commit-msg
+```
+
 - add `merge_lines.sh` as shell command `ml`
 
 ```sh
@@ -23,10 +29,4 @@ ln -s "$PWD/scripts/fix_page.sh" ~/.local/bin/fp
 - copy superscript numbers in `abbreviations` to register
 - copy ancient symbol and merged pages symbol from `dict.txt` to register
 - correct a page in `dict.txt`, use `ml` to merge lines
-- run checks until next line after page
-
-```
-./scripts/check.sh 99999
-```
-
-- commit with message `fix: 1/999`
+- commit with message `fix: 1/999`, the checks should run automatically
---
+++ scripts/commit_hook.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# exit immediately with a command's exit status if it fails
+set -e -u -o pipefail
+
+# filenames of staged files
+STAGED_FILES=$(git diff --name-only --cached --diff-filter=ACMR)
+
+# continue if only dict is staged
+if [[ "$STAGED_FILES" != "src/dict.txt" ]]
+then
+	exit 0
+fi
+
+# continue if only commit message is `fix: X/Y` for last page `X/Y`
+COMMIT_MSG=$(cat $1)
+if [[ ! "$COMMIT_MSG" =~ ^fix:\ [123]/[0-9]+$ ]]
+then
+	exit 0
+fi
+
+# get last page argument `X/Y` from commit message
+LAST_PAGE=$(echo $COMMIT_MSG | grep -Eo '[123]/[0-9]+')
+
+# call script/check.ts with last page as arguments
+./scripts/check.ts $LAST_PAGE
