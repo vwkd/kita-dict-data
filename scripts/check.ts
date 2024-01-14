5b70c3924ad52e9d8731348d77a3091be628b579
vwkd <33468089+vwkd@users.noreply.github.com>
2024-01-14 17:44:59 +0400

    scripts/check: extend

--- scripts/check.ts
+++ scripts/check.ts
@@ -259,7 +259,7 @@ function mergedLines(lines: Line[]): boolean {
   matches.push(...getMatches(lines, re_merged_lines6));
 
   // lowercase, hyphen, uppercase
-  const re_merged_lines7 = /(?!-(und|oder))[^a-zäöüßA-ZÄÖÜ][a-zäöüß]+-[A-ZÄÖÜ]/;
+  const re_merged_lines7 = /(?!(-(und|oder))|( irgendwann-Woche))[^a-zäöüßA-ZÄÖÜ][a-zäöüß]+-[A-ZÄÖÜ]/;
   matches.push(...getMatches(lines, re_merged_lines7));
 
   return printMatches(matches, "Merged lines");
